6c93032a783e81bf352d96355c19c5788d708ff5
Pritam Maharjan <primgdev@gmail.com>
2024-08-24 13:58:45 +1000

    installing tailwind css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Detail from './Components/Detail';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
       <Detail />
     </div>
   );
